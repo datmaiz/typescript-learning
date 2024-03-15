9af4719ef44b11e5bab92ffdf56383b377f86e24
datmaize <ngodat02012004@gmail.com>
2024-03-15 19:04:40 +0700

    chapter 3

--- src/chapter3/index.ts
+++ src/chapter3/index.ts
@@ -28,3 +28,33 @@ const student = {
 	firstName: 'firstname',
 	lastName: 'lastname',
 }
+
+const person: { firstName: string; lastName: string } = { firstName: 'myFirstName', lastName: 'myLastName' }
+
+class Person {
+	constructor(public firstName: string, public lastName: string) {
+		this.firstName = firstName
+		this.lastName = lastName
+	}
+}
+
+const newPerson = new Person('firstname', 'lastname')
+
+const myObject: { job?: string; name: string } = {
+	name: 'Mashle',
+}
+
+type Level = string
+
+const minLevel: Level = 'Low'
+
+type Cat = { name: string; purrs: boolean }
+type Dog = { name: string; barks: boolean; wags: boolean }
+
+type Animal = Cat | Dog
+type OrtherAnimal = Cat & Dog
+
+const numbers: number[] = [1, 2, 3, 4, 5]
+const animals: Animal[] = []
+
+const somethings: [number, number?][] = [[1, 2], [6]]
